9a439b426b8b05c060592e640c555ee1374e592f
SIMI <demogit222@gmail.com>
2017-09-13 16:34:09 +0700

    fdd third change

---
+++ Testcase.py
@@ -0,0 +1,14 @@
+# print("hello guys")
+# greetings = "Hello what's up"
+# Name = input("please enter your name")
+# print("you are welcome" + ' ' + Name)
+# Age = input("how old are you")
+# print("please confirm your age")
+# splitstring = "if you are sure type yes \nif not type no"
+# print(splitstring)
+print("the pet shop owner said \"he's resting\"")
+Asplitstring = "this is another split string but\.its not very long"
+print(Asplitstring)
+name = 'tim'
+age = 24
+print(name + str(age))
