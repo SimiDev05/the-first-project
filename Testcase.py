574021bfb76929c7d493afe8d0b80673c3dd75d0
SIMI <demogit222@gmail.com>
2017-09-14 10:03:59 +0700

    added lines

--- Testcase.py
+++ Testcase.py
@@ -12,3 +12,14 @@ print(Asplitstring)
 name = 'tim'
 age = 24
 print(name + str(age))
+
+print('there are 31 days in jan feb nov dec')
+jane = 'jan'
+faith = 'feb'
+_1st = 'mar'
+no = 31
+print('there are' + ' ' + str(no) + ' ' + 'days in' + ' ' + jane + ' ' + faith + ' ' + _1st)
+print('there are {0} days in {1}, {2}, {3}' .format(no, jane, faith, _1st))
+print('my age is {0} years and {1}' .format(24, 6))
+for i in range(1, 12):
+    print('No. {0} squared is {1} and cubed is {2}' .format(i, i**2, i**3))
